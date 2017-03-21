a0f6f1b51730e5d21ec9ff29ceaf3e41007d6353
Mikko Rinne <mikko.rinne@aalto.fi>
2017-03-21 18:35:40 +0200

    Add curl installation.

--- init.sh
+++ init.sh
@@ -3,20 +3,21 @@
 # Preparations for a new regular routes server
 # NOT NEEDED for updating an existing server
 
-echo "Installing a chef-client (https://www.chef.io/download-chef-client/)"
-echo "Sample location /opt/chef/ is assumed."
-
-curl -L https://www.chef.io/chef/install.sh | sudo bash
-
 echo "*****************"
 echo "Updating packages"
 
 sudo apt-get update
+sudo apt-get install curl
 
 echo "********************"
 echo "Installing libraries"
 
 sudo apt-get install build-essential python-pip python-dev gfortran libatlas-base-dev libblas-dev liblapack-dev libssl-dev
+
+echo "Installing a chef-client (https://www.chef.io/download-chef-client/)"
+echo "Sample location /opt/chef/ is assumed."
+
+curl -L https://www.chef.io/chef/install.sh | sudo bash
 
 echo "********************"
 echo "Create user 'lerero'"
